f666582fc934a29ee7961c94d5ba13d7883de458
Loomthie <97500065+Loomthie@users.noreply.github.com>
2022-09-20 21:05:38 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='TraceTable',
+    version='',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='Donald McFarland',
+    author_email='',
+    description='',
+    requires=[
+        "numpy",
+        "pandas",
+        "plotly"
+    ]
+)
