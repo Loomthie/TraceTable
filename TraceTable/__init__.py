fdd22ed07085190e4398a3040b53423bfd0504e4
Loomthie <mcfarldl@mail.uc.edu>
2022-09-27 13:23:30 -0400

    Added Stuff

--- TraceTable/__init__.py
+++ TraceTable/__init__.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import numpy as np
 
 
@@ -33,12 +34,83 @@ class TraceTable:
             new_df = pd.DataFrame(new_row)
             self.__df = pd.concat([self.__df, new_df], ignore_index=True)
 
-    def build_fig(self, x_key, y_key):
+    def build_fig(self, x=None, y=None):
         fig = go.Figure()
-        fig.update_xaxes(title=x_key)
-        fig.update_yaxes(title=y_key)
+        if x is not None and y is not None:
 
-        fig.add_trace(go.Scatter(x=self.__df[x_key], y=self.__df[y_key]))
+            if type(x) == str and type(y) == str:
+                fig = go.Figure()
+                fig.update_xaxes(title=x)
+                fig.update_yaxes(title=y)
+
+                fig.add_trace(go.Scatter(x=self.__df[x], y=self.__df[y]))
+
+            elif type(x) == str:
+                fig = make_subplots(rows=len(y),cols=1,
+                                    row_titles=y,column_titles=[x])
+                for row in range(len(y)):
+                    fig.add_trace(go.Scatter(x=self.__df[x],y=self.__df[y[row]]),row=row+1,col=1)
+
+            elif type(y) == str:
+                fig = make_subplots(rows=1,cols=len(x),
+                                    row_titles=[y],
+                                    column_titles=x)
+                for col in range(len(x)):
+                    fig.add_trace(go.Scatter(x=self.__df[x[col]],y=self.__df[y]),row=1,col=col+1)
+            else:
+                fig = make_subplots(rows=len(y),cols=len(x),
+                                    row_titles=y,
+                                    column_titles=x)
+                for row in range(len(y)):
+                    for col in range(len(x)):
+                        fig.add_trace(go.Scatter(x=self.__df[x[col]],y=self.__df[y[row]]),row=row+1,col=col+1)
+
+        elif x is not None:
+            if type(x) == list:
+                y = [key for key in self.__df]
+                fig = make_subplots(rows=len(y),cols=len(x),
+                                    row_titles=y,
+                                    column_titles=x)
+                for row in range(len(y)):
+                    for col in range(len(x)):
+                        fig.add_trace(go.Scatter(x=self.__df[x[col]],y=self.__df[y[row]]),row=row+1,col=col+1)
+            else:
+                y = [key for key in self.__df]
+                fig = make_subplots(rows=len(y),cols=1,
+                                    column_titles=[x],
+                                    row_titles=y)
+                for row in range(len(y)):
+                    fig.add_trace(go.Scatter(x=self.__df[x],y=self.__df[y[row]]),row=row+1,col=1)
+
+        elif y is not None:
+            if type(y) == list:
+                x = [key for key in self.__df]
+                fig = make_subplots(rows=len(y),cols=len(x),
+                                    row_titles=y,
+                                    column_titles=x)
+                for row in range(len(y)):
+                    for col in range(len(x)):
+                        fig.add_trace(go.Scatter(x=self.__df[x[col]],y=self.__df[y[row]]),row=row+1,col=col+1)
+            else:
+                x = [key for key in self.__df]
+                fig = make_subplots(rows=1,cols=len(x),
+                                    row_titles=[y],
+                                    column_titles=x)
+                for col in range(len(x)):
+                    fig.add_trace(go.Scatter(x=self.__df[x[col]],y=self.__df[y]),row=1,col=col+1)
+        else:
+            x = [key for key in self.__df]
+            y = [key for key in self.__df]
+
+            fig = make_subplots(rows=len(y),cols=len(x),
+                                row_titles=y,
+                                column_titles=x)
+
+            for row in range(len(y)):
+                for col in range(len(x)):
+                    fig.add_trace(go.Scatter(x=self.__df[x[col]],y=self.__df[y[row]]),row=row+1,col=col+1)
+
+        fig.update_layout(showlegend=False)
         return fig
 
     def display(self):
